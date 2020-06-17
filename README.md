ba5c0379289b439c58394bc5d3d892910f6595d6
letitia ho <letitiayhho@gmail.com>
2020-06-17 14:42:08 -0700

    Fix pop_saveset issue in preprocess_eeg_data.m, update preprocess wrapper to run preprocessing in background, flesh out README.md

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # talker_change_data_processing
 
-To run the preprocessing with the matlab script from command line with `preprocess_eeg_data.m` run `matlab -nodisplay -r "preprocess_eeg_data('<subject number>','<eeg data file name.raw>','<channel location file name.sfp')"`. e.g. `matlab -nodisplay -r "preprocess_eeg_data('302','302_eeg_data.raw','302_channel_locations.sfp')"`
+#### Run preprocessing
+
+To run the preprocessing script with the wrapper bash script use `./preprocess <subject_number>`. To see the MATLAB outputs run `tail -f <name of log file.log>` the log file name should come up after you run the preprocess script. Kill the process to stop viewing the output, type in the same `tail` command to see it again.
+
+To run the preprocessing manually with the matlab script from command line with `preprocess_eeg_data.m` run `matlab -nodisplay -r "preprocess_eeg_data('<subject number>','<eeg data file name.raw>','<channel location file name.sfp')"`. e.g. `matlab -nodisplay -r "preprocess_eeg_data('302','302_eeg_data.raw','302_channel_locations.sfp')"`
+
+##### File management
+
+Place all raw data files (`.raw` and `.sfp`) into a subdirectory called `raw_data`. Name all data files `<subject number>_eeg_data.raw` and `<subject_number>_channel_locations.sfp`.
--- preprocess_eeg_data.m
+++ preprocess_eeg_data.m
@@ -12,7 +12,7 @@ function [] = preprocess_eeg_data(subject_number, eeg_data_file_name, channel_lo
     cd('/Applications/eeglab2019')
     [ALLEEG EEG CURRENTSET ALLCOM] = eeglab;
     EEG.etc.eeglabvers = '2019.1'; % this tracks which version of EEGLAB is being used, you may ignore it
-    cd('/Applications/eeglab2019/uddin_preprocessing')
+    cd(fullfile('/Applications/eeglab2019/uddin_preprocessing', subject_number))
     
     %% 2. Import data
     fprintf(1, '\n\n2. Importing eeg data\n\n\n')
@@ -21,8 +21,9 @@ function [] = preprocess_eeg_data(subject_number, eeg_data_file_name, channel_lo
     EEG.setname = set_name;
     EEG = eeg_checkset(EEG);
     [ALLEEG EEG CURRENTSET] = eeg_store(ALLEEG, EEG); % copy it to ALLEEG
+    EEG = pop_saveset( EEG, 'filename', set_name);
         
-        2.1 Save original epoch order
+        % 2.1 Save original epoch order
         epoch_order_original = EEG.event;
         save('epoch_order_original');
         
@@ -36,39 +37,39 @@ function [] = preprocess_eeg_data(subject_number, eeg_data_file_name, channel_lo
     set_name = strcat(set_name, '_fil')
     EEG.setname = set_name;
     [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, CURRENTSET, 'setname', set_name); % copy changes to ALLEEG
-    return
+    EEG = pop_saveset( EEG, 'filename', set_name);
     
     % 4. Set channel locations
     fprintf(1, '\n\n4. Setting channel locations\n\n\n')
     EEG=pop_chanedit(EEG, 'lookup', fullfile('/Applications/eeglab2019/uddin_preprocessing/raw_data', channel_location_file_name),'setref',{'128' ''});
     EEG = eeg_checkset( EEG );
     
-        4.1 Re-reference to Cz
+        % 4.1 Re-reference to Cz
         fprintf(1, '\n\n4.1. Re-referencing data to Cz\n\n\n')
         EEG = pop_reref( EEG, 128,'keepref','on');
         set_name = strcat(set_name, '_rcz')
         EEG.setname = set_name;
     
-        4.2 Reject bad channels with clean_artifacts
+        % 4.2 Reject bad channels with clean_artifacts
         fprintf(1, '\n\n4.2. Rejecting bad channels with clean_artifacts\n\n\n')
         originalEEG = EEG; % keep original data for interpolation
         EEG = clean_artifacts(EEG, 'FlatlineCriterion',5,'ChannelCriterion',0.8,'LineNoiseCriterion',4,'Highpass','off','BurstCriterion','off','WindowCriterion','off','BurstRejection','on','Distance','Euclidian');
         set_name = strcat(set_name, '_chn')
         EEG.setname = set_name;
 
-        4.3 Interpolate channels
+        % 4.3 Interpolate channels
         fprintf(1, '\n\n4.3. Interpolating removed channels\n\n\n')
         EEG = pop_interp(EEG, originalEEG.chanlocs, 'spherical');
         set_name = strcat(set_name, '_int')
         EEG.setname = set_name;
         
-        4.4 Re-reference to average
+        % 4.4 Re-reference to average
         fprintf(1, '\n\n4.4. Re-referencing data to average\n\n\n')
         EEG = pop_reref( EEG, []);
         set_name = strcat(set_name, 'rav')
         EEG.setname = set_name;
         [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, CURRENTSET, 'setname', set_name);
-        
+        EEG = pop_saveset( EEG, 'filename', set_name);
    
     % 5. Clean continuous data
     fprintf(1, '\n\n5. Cleaning continuous data\n\n\n')
@@ -76,6 +77,7 @@ function [] = preprocess_eeg_data(subject_number, eeg_data_file_name, channel_lo
     set_name = strcat(set_name, '_cln')
     EEG.setname = set_name;
     [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, CURRENTSET, 'setname', set_name);
+    EEG = pop_saveset( EEG, 'filename', set_name);
     
     %% 6. Extract epochs
     fprintf(1, '\n\n6. Extracting epochs\n\n\n')
@@ -83,6 +85,7 @@ function [] = preprocess_eeg_data(subject_number, eeg_data_file_name, channel_lo
     set_name = strcat(set_name, '_epo')
     EEG.setname = set_name;
     [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, CURRENTSET, 'setname', set_name);
+    EEG = pop_saveset( EEG, 'filename', set_name);
     
     %% 7. Decompose data by ICA
     
@@ -109,7 +112,7 @@ function [] = preprocess_eeg_data(subject_number, eeg_data_file_name, channel_lo
         set_name = strcat(set_name, '_pru')
         EEG.setname = set_name;
         [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, CURRENTSET, 'setname', set_name);
-        EEG = pop_saveset( EEG, 'savemode','resave');
+        EEG = pop_saveset( EEG, 'filename', set_name);
 
         % 7.3 Save pruned epoch order
         epoch_order_pruned = EEG.event;
