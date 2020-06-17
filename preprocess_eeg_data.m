c2caa90ecb8bed707fc6ac714f67119a3b337b3c
letitia ho <letitiayhho@gmail.com>
2020-06-17 10:23:30 -0700

    Fixed ICA rank, updated README.md with instructions for running preprocessing code from command line

--- preprocess_eeg_data.m
+++ preprocess_eeg_data.m
@@ -21,61 +21,61 @@ function [] = preprocess_eeg_data(subject_number, eeg_data_file_name, channel_lo
     EEG.setname = set_name;
     EEG = eeg_checkset(EEG);
     [ALLEEG EEG CURRENTSET] = eeg_store(ALLEEG, EEG); % copy it to ALLEEG
-%         
-%         2.1 Save original epoch order
-%         epoch_order_original = EEG.event;
-%         save('epoch_order_original');
-%         
-%     % 3. Filter
-%     fprintf(1, '\n\n3. Filtering data from 0.3-400 Hz, notch filtering at 60 Hz\n\n\n')
-%     EEG = pop_eegfiltnew(EEG, 'locutoff',0.3,'hicutoff',400);
-%     EEG = pop_eegfiltnew(EEG, 'locutoff',59,'hicutoff',61,'revfilt',1);
-%     EEG = pop_eegfiltnew(EEG, 'locutoff',119,'hicutoff',121,'revfilt',1);
-%     EEG = pop_eegfiltnew(EEG, 'locutoff',179,'hicutoff',181,'revfilt',1);
-%     EEG = pop_eegfiltnew(EEG, 'locutoff',239,'hicutoff',241,'revfilt',1);
-%     set_name = strcat(set_name, '_fil')
-%     EEG.setname = set_name;
-%     [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, CURRENTSET, 'setname', set_name); % copy changes to ALLEEG
-%     return
-%     
-%     % 4. Set channel locations
-%     fprintf(1, '\n\n4. Setting channel locations\n\n\n')
-%     EEG=pop_chanedit(EEG, 'lookup', fullfile('/Applications/eeglab2019/uddin_preprocessing/raw_data', channel_location_file_name),'setref',{'128' ''});
-%     EEG = eeg_checkset( EEG );
-%     
-%         4.1 Re-reference to Cz
-%         fprintf(1, '\n\n4.1. Re-referencing data to Cz\n\n\n')
-%         EEG = pop_reref( EEG, 128,'keepref','on');
-%         set_name = strcat(set_name, '_rcz')
-%         EEG.setname = set_name;
-%     
-%         4.2 Reject bad channels with clean_artifacts
-%         fprintf(1, '\n\n4.2. Rejecting bad channels with clean_artifacts\n\n\n')
-%         originalEEG = EEG; % keep original data for interpolation
-%         EEG = clean_artifacts(EEG, 'FlatlineCriterion',5,'ChannelCriterion',0.8,'LineNoiseCriterion',4,'Highpass','off','BurstCriterion','off','WindowCriterion','off','BurstRejection','on','Distance','Euclidian');
-%         set_name = strcat(set_name, '_chn')
-%         EEG.setname = set_name;
-% 
-%         4.3 Interpolate channels
-%         fprintf(1, '\n\n4.3. Interpolating removed channels\n\n\n')
-%         EEG = pop_interp(EEG, originalEEG.chanlocs, 'spherical');
-%         set_name = strcat(set_name, '_int')
-%         EEG.setname = set_name;
-%         
-%         4.4 Re-reference to average
-%         fprintf(1, '\n\n4.4. Re-referencing data to average\n\n\n')
-%         EEG = pop_reref( EEG, []);
-%         set_name = strcat(set_name, 'rav')
-%         EEG.setname = set_name;
-%         [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, CURRENTSET, 'setname', set_name);
-%         
-%    
-%     % 5. Clean continuous data
-%     fprintf(1, '\n\n5. Cleaning continuous data\n\n\n')
-%     EEG = clean_artifacts(EEG, 'FlatlineCriterion','off','ChannelCriterion','off','LineNoiseCriterion','off','Highpass','off','BurstCriterion',20,'WindowCriterion',0.25,'BurstRejection','on','Distance','Euclidian','WindowCriterionTolerances',[-Inf 7] );
-%     set_name = strcat(set_name, '_cln')
-%     EEG.setname = set_name;
-%     [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, CURRENTSET, 'setname', set_name);
+        
+        2.1 Save original epoch order
+        epoch_order_original = EEG.event;
+        save('epoch_order_original');
+        
+    % 3. Filter
+    fprintf(1, '\n\n3. Filtering data from 0.3-400 Hz, notch filtering at 60 Hz\n\n\n')
+    EEG = pop_eegfiltnew(EEG, 'locutoff',0.3,'hicutoff',400);
+    EEG = pop_eegfiltnew(EEG, 'locutoff',59,'hicutoff',61,'revfilt',1);
+    EEG = pop_eegfiltnew(EEG, 'locutoff',119,'hicutoff',121,'revfilt',1);
+    EEG = pop_eegfiltnew(EEG, 'locutoff',179,'hicutoff',181,'revfilt',1);
+    EEG = pop_eegfiltnew(EEG, 'locutoff',239,'hicutoff',241,'revfilt',1);
+    set_name = strcat(set_name, '_fil')
+    EEG.setname = set_name;
+    [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, CURRENTSET, 'setname', set_name); % copy changes to ALLEEG
+    return
+    
+    % 4. Set channel locations
+    fprintf(1, '\n\n4. Setting channel locations\n\n\n')
+    EEG=pop_chanedit(EEG, 'lookup', fullfile('/Applications/eeglab2019/uddin_preprocessing/raw_data', channel_location_file_name),'setref',{'128' ''});
+    EEG = eeg_checkset( EEG );
+    
+        4.1 Re-reference to Cz
+        fprintf(1, '\n\n4.1. Re-referencing data to Cz\n\n\n')
+        EEG = pop_reref( EEG, 128,'keepref','on');
+        set_name = strcat(set_name, '_rcz')
+        EEG.setname = set_name;
+    
+        4.2 Reject bad channels with clean_artifacts
+        fprintf(1, '\n\n4.2. Rejecting bad channels with clean_artifacts\n\n\n')
+        originalEEG = EEG; % keep original data for interpolation
+        EEG = clean_artifacts(EEG, 'FlatlineCriterion',5,'ChannelCriterion',0.8,'LineNoiseCriterion',4,'Highpass','off','BurstCriterion','off','WindowCriterion','off','BurstRejection','on','Distance','Euclidian');
+        set_name = strcat(set_name, '_chn')
+        EEG.setname = set_name;
+
+        4.3 Interpolate channels
+        fprintf(1, '\n\n4.3. Interpolating removed channels\n\n\n')
+        EEG = pop_interp(EEG, originalEEG.chanlocs, 'spherical');
+        set_name = strcat(set_name, '_int')
+        EEG.setname = set_name;
+        
+        4.4 Re-reference to average
+        fprintf(1, '\n\n4.4. Re-referencing data to average\n\n\n')
+        EEG = pop_reref( EEG, []);
+        set_name = strcat(set_name, 'rav')
+        EEG.setname = set_name;
+        [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, CURRENTSET, 'setname', set_name);
+        
+   
+    % 5. Clean continuous data
+    fprintf(1, '\n\n5. Cleaning continuous data\n\n\n')
+    EEG = clean_artifacts(EEG, 'FlatlineCriterion','off','ChannelCriterion','off','LineNoiseCriterion','off','Highpass','off','BurstCriterion',20,'WindowCriterion',0.25,'BurstRejection','on','Distance','Euclidian','WindowCriterionTolerances',[-Inf 7] );
+    set_name = strcat(set_name, '_cln')
+    EEG.setname = set_name;
+    [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, CURRENTSET, 'setname', set_name);
     
     %% 6. Extract epochs
     fprintf(1, '\n\n6. Extracting epochs\n\n\n')
