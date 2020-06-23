51cde6e04827e4b4d39eed02e81fc7ca1610a151
letitia ho <letitiayhho@gmail.com>
2020-06-23 14:00:47 -0700

    Update convolve_and_cross_correlate.m to match new file organization, create wrapper to run that file

--- src/convolve_and_cross_correlate.m
+++ src/convolve_and_cross_correlate.m
@@ -1,15 +1,15 @@
-% function [] = convolve_and_cross_correlate(subject_number)
-subject_number = '302';
+function [] = convolve_and_cross_correlate(subject_number)
 
-if 1
+    fprintf(1, strcat('Analyzing data from subject #', subject_number))
+
     %% 1. Import data
-    addpath('./analysis')
-    addpath(fullfile('./preprocessing', subject_number))
-    addpath('./preprocessing/stim')
+    % Working from /Applications/eeglab2019/talker-change-data-processing
+    addpath(fullfile('data', subject_number)) % add subject data to path
+    addpath(fullfile('data/stim')) % add audio stimuli directory to path
 
     % Import EEG data
-    eeg_data = load('preprocessed_eeg_data');
-    eeg_data = eeg_data.('preprocessed_eeg_data');
+    eeg_data = load('eeg_data');
+    eeg_data = eeg_data.('eeg_data');
 
     % Import original epoch order
     epoch_order_original = load('epoch_order_original');
@@ -40,7 +40,7 @@ if 1
             j = j+1;
         end
     end
-    
+
     % Sort pruned epoch order by latency
     epoch_order_pruned = sortrows(epoch_order_pruned, 'latency');
 
@@ -48,15 +48,15 @@ if 1
     % Initialize data tables
     convolution = zeros(size(eeg_data, 3), size(eeg_data, 1));
     cross_correlation = zeros(size(eeg_data, 3), size(eeg_data, 1));
-    
+
     % Loop over channels
     for i = 1:size(eeg_data, 1)
         disp(strcat('Channel #', num2str(i)))
-        
+
         % Loop over epochs
          for j = 1:size(eeg_data, 3)
              epoch = eeg_data(i, :, j);
-             
+
              % Load stimuli .wav file for epoch
              word = char(epoch_order_pruned.word(j));
              auditory_stimuli = audioread(word);
@@ -66,24 +66,27 @@ if 1
              cross_correlation(j, i) = mean(xcorr(epoch, auditory_stimuli)); % should be #stim x #channels
          end
     end
-    
+
     %% 4. Write data
     % Add relevant info to data tables
     convolution = array2table(convolution);
     convolution_data_table = table([epoch_order_pruned.type],...
-        [epoch_order_pruned.epoch],... 
+        [epoch_order_pruned.epoch],...
         [epoch_order_pruned.word],...
         [convolution],...
         'VariableNames', {'condition', 'epoch', 'word', 'convolution'});
-    
+
     cross_correlation = array2table(cross_correlation);
     cross_correlation_data_table = table([epoch_order_pruned.type],...
-        [epoch_order_pruned.epoch],... 
+        [epoch_order_pruned.epoch],...
         [epoch_order_pruned.word],...
         [cross_correlation],...
         'VariableNames', {'condition', 'epoch', 'word', 'convolution'});
 
     % Write data
-    save('convolution_data_table', 'convolution_data_table')
-    save('cross_correlation_data_table', 'cross_correlation_data_table')
+    save(fullfile('data', subject_number, 'convolution_data_table'), 'convolution_data_table')
+    save(fullfile('data', subject_number, 'cross_correlation_data_table'), 'cross_correlation_data_table')
+
+    %% Quit
+    quit
 end
