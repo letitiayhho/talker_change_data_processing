0204d12db4bc4f8328dd3cd03d3ff460aa734df7
letitia ho <letitiayhho@gmail.com>
2020-06-28 06:44:15 -0700

    Edited function heading to make it more informative

--- src/analyze.m
+++ src/analyze.m
@@ -1,33 +1,46 @@
-function [] = analyze(method, channels)
-% Computes average cross-correlation between eeg signal and audio 
-% stimuli across all subjects, channels and trials for each condition
+function [condition_means] = analyze(method, channels)
+% DESCRIPTION:
+%   Computes average cross-correlation between eeg signal and audio 
+%   stimuli across all subjects, channels and trials for each condition
+%
+% INPUT:
+%   method - (char) 'cross_correlation' or 'convolution'
+%   channels - (double) array containing the channels you wish you analyze
+%           [34; 38] for anterior temporal channels
+%           [40; 44; 45; 46] for central temporal channels
+%           [:] for all channels
 
-% Passing channels into the function e.g. [34; 38] for anterior temporal 
-% channels, [40; 44; 45; 46] for central temporal channels. [:] for all
-% channels.
+%     method = 'cross_correlation';
+%     channels = [34; 38];
 
     %% Main
     % Split four-letter condition code up into individual arrays for each IV
-    [split_conditions] = get_split_conditions();
+    [split_conditions] = get_split_conditions(method);
 
     % Get subject means
-    [subject_means] = get_subject_means(method, channels, split_conditions);
+    [subject_means, two_way_subject_means] = get_subject_means(method, channels, split_conditions);
 
     % Get summary statistics
-    [condition_means] = get_summary_statistics(subject_means);
+    [condition_means] = get_summary_statistics(subject_means)
 
     % Run a Three-Way ANOVA
-    [t] = get_three_way_anova(subject_means)
+%     [t] = get_three_way_anova(subject_means)
+    
+    % Run Friedman's non-parametric test
+%     [p] = get_friedmans(two_way_subject_means)
+
+    % Run a permutation test
+%     [p] = get_permutation_test(subject_means)
 
     %% Helper functions
         %% Load data of a single subject
         function [data] = load_single_subject_data(method, i)
-            if method == 'cross_correlation'
+            if strcmp(method, 'cross_correlation')
                 data_files = dir('data/**/cross_correlation_data_table.mat');
                 data_table_full_path = fullfile(data_files(i).folder, 'cross_correlation_data_table.mat');
                 data = load(data_table_full_path);
                 data = data.('cross_correlation_data_table');
-            elseif method == 'convolution'
+            elseif strcmp(method, 'convolution')
                 data_files = dir('data/**/convolution_data_table.mat');
                 data_table_full_path = fullfile(data_files(i).folder, 'convolution_data_table.mat');
                 data = load(data_table_full_path);
@@ -36,13 +49,13 @@ function [] = analyze(method, channels)
         end
 
         %% Split four-letter condition code up into individual arrays for each IV
-        function [split_conditions] = get_split_conditions()
+        function [split_conditions] = get_split_conditions(method)
                 % G/S: general (i.e. low) vs specific (i.e. high) constraint sentence stems
                 % M/N: meaningful vs nonsense ending word in the context of the rest of the sentence
                 % S/T: same vs different talker in the ending word
 
             % Conditions
-            data = load_single_subject_data(1);
+            data = load_single_subject_data(method, 1);
             conditions = unique(data.condition);
 
             % Preallocate memory for iv arrays
@@ -62,14 +75,14 @@ function [] = analyze(method, channels)
         end
 
         %% Get subject means
-        function [subject_means] = get_subject_means(method, channels, split_conditions)
-            if method == 'cross_correlation'
-                number_of_subjects = size(dir('data/**/cross_correlation_data_table.mat'));
-                data = load_single_subject_data(1);
+        function [subject_means, two_way_subject_means] = get_subject_means(method, channels, split_conditions)
+            if strcmp(method, 'cross_correlation')
+                number_of_subjects = size(dir('data/**/cross_correlation_data_table.mat'), 1);
+                data = load_single_subject_data(method, 1);
                 number_of_conditions = size(unique(data.condition), 1);
-            elseif method == 'convolution'
-                number_of_subjects = size(dir('data/**/convolution.mat'));
-                data = load_single_subject_data(1);
+            elseif strcmp(method, 'convolution')
+                number_of_subjects = size(dir('data/**/convolution_data_table.mat'), 1);
+                data = load_single_subject_data(method, 1);
                 number_of_conditions = size(unique(data.condition), 1);
             end
             
@@ -79,6 +92,7 @@ function [] = analyze(method, channels)
             meaning = char(zeros(length, 1));
             talker = char(zeros(length, 1));
             means = zeros(length, 1);
+            two_way_subject_means = zeros(number_of_subjects, number_of_conditions);
 
             % Interate across subjects
             for i = 1:number_of_subjects
@@ -87,7 +101,7 @@ function [] = analyze(method, channels)
                 for j = 1:number_of_conditions
 
                     % Load each subject's data and turn into table for easy manipulation
-                    data = load_single_subject_data(i);
+                    data = load_single_subject_data(method, i);
                     data_expanded = table2array(data.convolution);
 
                     % Get means for each channel
@@ -99,7 +113,7 @@ function [] = analyze(method, channels)
                         trial_means(trial) = mean(data_of_one_channel(trial, :));
                     end
 
-                    % Get condition means within each subject
+                    % Get means for each condition
                     data.trial_means = trial_means;
                     group_means = groupsummary(data,...
                         'condition',...
@@ -107,21 +121,23 @@ function [] = analyze(method, channels)
                         'trial_means');
                     group_means.Properties.VariableNames{3} = 'group_means';
 
-                    % Write values to iv arrays
+                    % Write values to iv arrays for ANOVA
                     constraint((j-1)*11+i) = split_conditions.constraint(j);
                     meaning((j-1)*11+i) = split_conditions.meaning(j);
                     talker((j-1)*11+i) = split_conditions.talker(j);
                     means((j-1)*11+i) = group_means.group_means(j);
+                    
+                    % Write two-way table for Friedman's non-parametric test
+                    two_way_subject_means(i, :) = group_means.group_means;
                 end
             end
             
-            % Create data table
+            % Combine iv arrays into data table for ANOVA
             subject_means = table(constraint, meaning, talker, means);
         end
 
         %% Get summary statistics
         function [condition_means] = get_summary_statistics(subject_means)
-            % Compute average for each condition
             condition_means = grpstats(subject_means,...
                 {'constraint', 'meaning', 'talker'},...
                 {'mean', 'std'},...
@@ -142,5 +158,11 @@ function [] = analyze(method, channels)
                 'interaction',...
                 'varnames',...
                 {'constraint','meaning','talker'});
+        end
+    
+        %% Run Friedman's non-parametric test
+        function [p] = get_friedmans(two_way_subject_means)
+            number_of_subjects = size(two_way_subject_means, 1);
+            p = friedman(two_way_subject_means, number_of_subjects);
         end
 end
