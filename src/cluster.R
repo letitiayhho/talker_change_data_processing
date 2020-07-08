9cf2bcdb4c67242da012f8d72ec41d5a4c39f59a
letitia ho <letitiayhho@gmail.com>
2020-07-08 13:19:20 -0700

    Unfinished, trying to get clustering right

--- src/cluster.R
+++ src/cluster.R
@@ -13,17 +13,16 @@
   t_values_raw = read.csv("data/cluster_t_values.csv")
   coords_raw = read.delim("data/302/channel_locations.sfp", header = FALSE, sep = "", dec = ".") %>%
     .[startsWith(as.character(.$V1), "E"), ]
-  # coords_raw = coords_raw[startsWith(as.character(coords_raw$V1), "E"), ]
-  
+
   ## Get pairwise Euclidean distance
   coords = cbind(coords_raw$V2, coords_raw$V3, coords_raw$V4)
   distances = as.matrix(dist(coords))
   
   ## Get histogram of pairwise distances
-  sort_distances = as.vector(distances) %>%
-    .[!duplicated(.)] %>%
-    sort() %>%
-    hist(., breaks = 50, main = 'Histogram of pairwise distances')
+  # sort_distances = as.vector(distances) %>%
+  #   .[!duplicated(.)] %>%
+  #   sort() %>%
+  #   hist(., breaks = 50, main = 'Histogram of pairwise distances')
 
   ## Get t_values
   if (condition == 'constraint'){
@@ -43,14 +42,43 @@
     above_threshold_neighbors[i] = above_threshold_neighbors_for_each_channel
   }
 
-  clusters = list()
+  clusters = vector(mode = "list")
   # List 1: above threshold neighbors for each channel
   # List 2: compiled clusters
 
-  for (i in 1:nrow(distances)) {
+  # Loop through neighbors
+  for (i in 1:length(above_threshold_neighbors)) {
+    
+    # Continue if neighbors is an empty list
+    if (length(above_threshold_neighbors[[i]]) == 0) {next}
+    
+    # Check if there is overlap with any cluster
+    
+      # Create one long vector of all clusters
+    
+    
+    
+      
+    # Loop through existing clusters to identify cluster with overlap
+    for (j in 1:nrow(clusters)) {
+      
+      # Add each neighbor to each cluster and see if there are duplicates
+      comparison = c(above_threshold_neighbors[[i]], clusters[[j]])
+
+      if (FALSE %in% duplicated(comparison)) {  # If there are no duplicates create a new cluster with the neighbors
+        cluster = above_threshold_neighbors[[i]]
+      } else if (TRUE %in% duplicated(duplicates)) { # If there are duplicates keep the cluster merged and remove duplicates
+        cluster = unique(comparison)
+      }
+      clusters = c(clusters, cluster)
+    }
+  }
+    
+    
+    
     # Read through each value in the clusters
     # Loop through values within each cluster
       # If read value is in a cluster,
-    final_list = c(final_list, cluster)
-  }
+    # final_list = c(final_list, cluster)
+  # }
 
