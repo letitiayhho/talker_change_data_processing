3d0847bf52f1f9710a2d61380a41c80c22436f6c
letitia ho <letitiayhho@gmail.com>
2020-07-08 10:08:39 -0700

    Created R-script for identifying clusters of significant channels

---
+++ src/cluster.R
@@ -0,0 +1,56 @@
+# get_clusters = function(distance, min_cluster_size, t_threshold) {
+
+  ## RECOMMENDED DISTANCE = 5 (see histogram)
+  distance = 4
+  t_threshold = 2.2281 # 0.05
+  ## 0.1, 0.01 or 0.05, enter DF (n-1), enter alpha, get two-sided t
+  condition = "talker"
+  
+  library(dplyr)
+  
+  ## Load data
+  setwd("/Applications/eeglab2019/talker-change-data-processing")
+  t_values_raw = read.csv("data/cluster_t_values.csv")
+  coords_raw = read.delim("data/302/channel_locations.sfp", header = FALSE, sep = "", dec = ".") %>%
+    .[startsWith(as.character(.$V1), "E"), ]
+  # coords_raw = coords_raw[startsWith(as.character(coords_raw$V1), "E"), ]
+  
+  ## Get pairwise Euclidean distance
+  coords = cbind(coords_raw$V2, coords_raw$V3, coords_raw$V4)
+  distances = as.matrix(dist(coords))
+  
+  ## Get histogram of pairwise distances
+  sort_distances = as.vector(distances) %>%
+    .[!duplicated(.)] %>%
+    sort() %>%
+    hist(., breaks = 50, main = 'Histogram of pairwise distances')
+
+  ## Get t_values
+  if (condition == 'constraint'){
+    t_values = t_values_raw[1,]
+  } else if (condition == 'meaning') {
+    t_values = t_values_raw[2,]
+  } else if (condition == 'talker') {
+    t_values = t_values_raw[3,]
+  }
+
+  ## Get clusters
+  above_threshold_neighbors = vector(mode = "list", length = nrow(distances))
+  for (i in 1:nrow(distances)) {
+    neighboring_channels = which(distances[i,] < distance)
+    above_threshold_neighbors_indexes = which(abs(t_values[neighboring_channels]) > 2)
+    above_threshold_neighbors_for_each_channel = list(neighboring_channels[above_threshold_neighbors_indexes])
+    above_threshold_neighbors[i] = above_threshold_neighbors_for_each_channel
+  }
+
+  clusters = list()
+  # List 1: above threshold neighbors for each channel
+  # List 2: compiled clusters
+
+  for (i in 1:nrow(distances)) {
+    # Read through each value in the clusters
+    # Loop through values within each cluster
+      # If read value is in a cluster,
+    final_list = c(final_list, cluster)
+  }
+
