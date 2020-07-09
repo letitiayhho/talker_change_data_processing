1467408f1468af7f88e29677ffef58f267c748f9
letitia ho <letitiayhho@gmail.com>
2020-07-09 16:00:11 -0700

    Created new implementation for actual clustering with a recursive loop

--- src/clustering.R
+++ src/clustering.R
@@ -1,7 +1,7 @@
 get_clusters <- function(condition,
                          sliding_method = "cross_correlation",
                          clustering_method = "euclidean",
-                         distance = 3.5,
+                         max_distance = 3.5,
                          alpha = 0.05,
                          n = 11,
                          min_cluster_size = 2) {
@@ -13,7 +13,7 @@ get_clusters <- function(condition,
   ##  condition (string) - "constraint", "meaning", or "talker"
   ##  sliding_method (string) - "cross_correlation", "convolution" (optional, default = "cross_correlation")
   ##  clustering_method (string) - "euclidean", "rank", or "density" (optional, default = "euclidean")
-  ##  distance (double) - distance between neighboring electrodes, see histogram function (optional, default = 3.5)
+  ##  max_distance (double) - max_distance between neighboring electrodes, see histogram function (optional, default = 3.5)
   ##  alpha (double) - significance level at which to include channels in clusters (optional, default = 0.05)
   ##  n (int) - sample size (optional, default = 11)
   ##  min_cluster_size (int) - smallest cluster size to output (optional, default = 2)
@@ -21,6 +21,16 @@ get_clusters <- function(condition,
   ## OUTPUT:
   ##  (list) - list of lists containing the numbers of channels in each cluster
   
+
+  # ## PARAMETERS (TEMP):
+  # condition = "talker"
+  # sliding_method = "cross_correlation"
+  # clustering_method = "euclidean"
+  # max_distance = 3.5
+  # alpha = 0.05
+  # n = 11
+  # min_cluster_size = 2
+
   
   ## FUNCTIONS:
   get_t_values <- function(condition, sliding_method) {
@@ -68,104 +78,95 @@ get_clusters <- function(condition,
       hist(., breaks = 50, main = "Histogram of pairwise distances")
   }
   
-
-  actual_get_clusters <- function(distance, alpha, n, min_cluster_size, distances, t_values) {
-    
-    # Determine threshold for t-values based on specified alpha significance level
+  
+  get_neighboring_clusters <- function(max_distance, alpha, n, distances) {
+    # Determine threshold for t-values based on specified alpha level
     t_threshold <- qt(1-(alpha/2), df = n-1)
     
-    # Identify neighboring channels of each electrode with t-values above threshold
-    above_threshold_neighbors <- vector(mode = "list", length = nrow(distances))
+    # Identify neighboring above-threshold channels of all above-threshold channels
+    clusters <- vector(mode = "list")
     for (i in 1:nrow(distances)) {
       
-      # Identify neighboring channels within the specified radius
-      neighboring_channels = which(distances[i, ] < distance)
-      
-      # Subsetting the channels
-      above_threshold_neighbors_indexes = which(abs(t_values[neighboring_channels]) > t_threshold)
-      above_threshold_neighbors_for_each_channel = list(neighboring_channels[above_threshold_neighbors_indexes])
-      above_threshold_neighbors[i] = above_threshold_neighbors_for_each_channel
-    }
-    
-    # Loop through neighbors
-    clusters <- vector(mode = "list")
-    for (i in 1:length(above_threshold_neighbors)) {
-      
-      # Continue if neighbors is one channel or fewer
-      if (length(above_threshold_neighbors[[i]]) <= 1) {
-        next
-      }
-      
-      # Add to clusters if clusters is empty
-      if (length(clusters) == 0) {
-        cluster = above_threshold_neighbors[[i]]
-        clusters = list(c(cluster))
-      }
-      
-      print('<------ new neighbor ------>') # CHECK
-      
-      # Loop through existing clusters to identify cluster with overlap
-      for (j in 1:length(clusters)) {
+      # Check whether channel itself is above threshold
+      if (abs(t_values[i]) > t_threshold) {
         
-        # Add each neighbor to each cluster and see if there are duplicates
-        comparison = c(clusters[[j]], above_threshold_neighbors[[i]])
+        # Identify all neighboring channels within the specified max_distance
+        neighboring_channels = which(distances[i, ] < max_distance)
         
-        print('neighbors:')
-        print(above_threshold_neighbors[[i]]) # CHECK
-        print('current cluster:')
-        print(clusters[[j]]) # CHECK
+        # Keep above-threshold neighbors
+        indexes = which(abs(t_values[neighboring_channels]) > t_threshold)
+        clusters_for_one_channel = list(neighboring_channels[indexes])
         
-        # If there are duplicates keep the cluster merged and remove duplicates
-        if (TRUE %in% duplicated(comparison)) {
-          cluster = unique(comparison)
-          # clusters[[j]] <- NA
-          break
-          
-          # If there are no duplicates create a new cluster with the neighbors
-        } else {
-          cluster <- above_threshold_neighbors[[i]]
+        # Exclude channels whose only active neighbor is itself
+        if (length(clusters_for_one_channel[[1]]) > 1) {
+          clusters = c(clusters, clusters_for_one_channel)
         }
-      }
-      
-      print('adding cluster:')
-      print(cluster)
-      
-      # Add cluster to list of clusters
-      clusters <- c(clusters, list(cluster))
+      } 
     }
-    
-    # Replace clusters smaller than 2 with NA
-    for (i in 1:length(clusters)) {
-      if (length(clusters[[i]]) < 2) {
-        clusters[[i]] <- NA
-      }
-    }
-    
-    # Remove NAs
-    # clusters <- clusters[!is.na(clusters)]
-    
-    # Turn into matrix
-    clusters <- as.matrix(clusters)
-    
     # Return
     return(clusters)
   }
-  
-  
-  ## MAIN:
-  t_values <- get_t_values(condition, sliding_method)
-  coordinates <- get_coordinates()
-  distances <- get_pairwise_distances(coordinates)
-  get_histogram_of_pairwise_distances(distances)
-  clusters <- actual_get_clusters(distance, alpha, n, min_cluster_size, distances, t_values)
 
-  ## SAVE:
-  file_name <- paste(sliding_method, "_", condition, "_clusters.txt", sep = "")
-  for (i in 1:length(clusters)) {
-    write(clusters[[i]], file = file_name, append = TRUE, ncolumns = 128)
+    get_actual_clusters <- function(clusters) {
+      
+      # Compare every cluster to every other cluster
+      for (i in 1:length(clusters)) {
+        cluster_a <- clusters[[i]]
+        
+        # Loop through every cluster
+        for (j in 1:length(clusters)) {
+          cluster_b <- clusters[[j]]
+          
+          # Compare the two clusters...
+          combined <- c(cluster_a, cluster_b)
+          
+          # If there are duplicates...
+          if (TRUE %in% duplicated(combined) & (j > i)) { 
+            
+            # Set the original cluster into the merged cluster
+            clusters[[i]] <- unique(combined)
+            
+            # Set the compared cluster to null
+            clusters[[j]] <- 0
+          }
+        }
+      }
+        
+      # Remove all clusters smaller than min_cluster_size, remove 0s
+      keep = vector(mode = "list")
+      for (i in 1:length(clusters)) {
+        cluster = clusters[[i]]
+        if (length(cluster) >= min_cluster_size) {
+          keep <- c(keep, list(cluster))
+        }
+      }
+      clusters <- keep
+      
+      # Return if all possible clusters are created
+      if (!(TRUE %in% duplicated(unlist(clusters)))) {
+        return(clusters)
+      }
+      
+      # Recursively apply function
+      return(get_actual_clusters(clusters))
+    }
+
+  
+    ## MAIN:
+    t_values <- get_t_values(condition, sliding_method)
+    coordinates <- get_coordinates()
+    distances <- get_pairwise_distances(coordinates)
+    # get_histogram_of_pairwise_distances(distances)
+    neighboring_clusters <- get_neighboring_clusters(max_distance, alpha, n, distances)
+    clusters <- get_actual_clusters(neighboring_clusters)
+    
+    
+    # SAVE:
+    file_name <- paste(sliding_method, "_", condition, "_clusters.txt", sep = "")
+    file.remove(file_name)
+    for (i in 1:length(clusters)) {
+      write(clusters[[i]], file = file_name, append = TRUE, ncolumns = 128)
+    }
   }
-
-  return(clusters)
-}
 
 
