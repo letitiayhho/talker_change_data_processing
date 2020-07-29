6ca01392bc2e9d50cd385a917d187ca1a5fea8bc
letitia ho <letitiayhho@gmail.com>
2020-07-29 16:56:41 -0700

    Commit some formatting changes before I use modified label4mni on it

--- src/get_mni_cortical_areas.R
+++ src/get_mni_cortical_areas.R
@@ -1,4 +1,16 @@
-get_mni_cortical_areas <- function(save_file = "mni_coordinates_areas_2.txt") {
+get_mni_cortical_areas <- function(radius = 5, save_file = NULL) {
+  #' DESCRIPTION:
+  #'  Get closest cortical areas to each channel based on its MNI coordinate
+  #'
+  #' INPUT:
+  #'  radius (int) - radius around channel to search for labeled cortical areas
+  #'  save_file (char) - name of the file to write labels to (optional, default - NULL)
+  #'
+  #' OUTPUT:
+  #'  (table) - table containing the original mni coordinates, aal.label, aal.distance,
+  #'    ba.label, ba.distance of each channel
+  
+  ## FUNCTIONS:
   get_coordinates <- function() {
     coordinates_fp <- "/Applications/eeglab2019/talker-change-data-processing/data/aggregate/mni_coordinates.txt"
     coordinates <- read.delim(coordinates_fp, header = FALSE, sep = "", dec = ".") %>%
@@ -6,18 +18,21 @@ get_mni_cortical_areas <- function(save_file = "mni_coordinates_areas_2.txt") {
     colnames(coordinates) <- c("channels", "y", "x", "z")
     return(coordinates) }
   
-  # Load in packages and data
+  ## SOURCE:
+  library(dplyr)
   library(label4MRI)
   source('/Applications/eeglab2019/talker-change-data-processing/src/get_mni_coordinates.R', echo=TRUE)
 
-  # Load coordinates
+  ## MAIN:
   coordinates <- get_coordinates()
-  
-  # Get labels
   labels <- t(mapply(FUN = mni_to_region_name, x = coordinates$x, y = coordinates$y, z = coordinates$z))
   
-  # Save
+  ## SAVE:
   coordinates = cbind(coordinates, labels)
-  save_file_fp = file.path("/Applications/eeglab2019/talker-change-data-processing/data/aggregate/", save_file)
-  write.table(format(coordinates, digits = 2), file = save_file_fp, append = FALSE, sep = "\t", col.names = TRUE, quote = FALSE)
+  if (!is.null(save_file)) {
+    save_file_fp = file.path("/Applications/eeglab2019/talker-change-data-processing/data/aggregate/", save_file)
+    write.table(format(coordinates, digits = 2), file = save_file_fp, append = FALSE, sep = "\t", col.names = TRUE, quote = FALSE)
   }
+  
+  return(coordinates)
+}
