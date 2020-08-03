c4aed66991bbe6cf742e3ded8a89f8b3eb0305e0
letitia ho <letitiayhho@gmail.com>
2020-08-03 14:18:19 -0700

    Added formant column to output data

--- src/convolve_and_cross_correlate_formants.m
+++ src/convolve_and_cross_correlate_formants.m
@@ -4,7 +4,7 @@ function [] = convolve_and_cross_correlate_formants(subject_number)
     %% 1. Import data
     cd('/Applications/eeglab2019/talker-change-data-processing')
     addpath(fullfile('data', subject_number)) % add subject data to path
-    addpath(fullfile('data/stim')) % add audio stimuli directory to path
+    addpath(fullfile('data/stim/formants')) % add audio stimuli directory to path
     
     % Import EEG data
     eeg_data = load('eeg_data');
@@ -60,21 +60,22 @@ function [] = convolve_and_cross_correlate_formants(subject_number)
     % Loop over formants
     for i = 1:length(formants)
         formant = formants(i);
+        fprintf(1, ['Correlating eeg data with audio files filtered for ', char(formant), '\n'])
     
         % Loop over channels
         for j = 1:size(eeg_data, 1)
-            disp(strcat('Channel #', num2str(j)))
+            fprintf(1, ['Channel #', num2str(j), '\n'])
 
             % Loop over epochs
              for k = 1:size(eeg_data, 3)
                  epoch = eeg_data(j, :, k);
 
                  % Load stimuli .wav file for epoch
-                 word = strcat(erase(char(epoch_order_pruned.word(k)), ".wav"),...
-                     "_", formant, ".wav");
+                 word = strcat(erase(char(epoch_order_pruned.word(k)), ".wav"), "_", formant, ".wav");
                  auditory_stimuli = audioread(word);
 
                  % Compute convolution and cross correlation
+                 cross_correlation(k, j, i) = i+j+k;
                  cross_correlation(k, j, i) = mean(xcorr(epoch, auditory_stimuli)); % should be #stim x #channels
 
              end
@@ -83,29 +84,39 @@ function [] = convolve_and_cross_correlate_formants(subject_number)
 
     %% 4. Write data
     % Add relevant info to data tables
-    cross_correlation_data_table = save_corr(cross_correlation, epoch_order_pruned, formant, formants)
+    f0_cross_correlation_data_table = save_corr(cross_correlation, epoch_order_pruned, 'f0', formants);
+    f1_cross_correlation_data_table = save_corr(cross_correlation, epoch_order_pruned, 'f1', formants);
+    f2_cross_correlation_data_table = save_corr(cross_correlation, epoch_order_pruned, 'f2', formants);
+    f3_cross_correlation_data_table = save_corr(cross_correlation, epoch_order_pruned, 'f3', formants);
     
-    function [cross_correlation_data_table] = save_corr(cross_correlation, formant, formants)
+    % Write data
+    cross_correlation_data_table = [f0_cross_correlation_data_table;
+        f0_cross_correlation_data_table; 
+        f0_cross_correlation_data_table; 
+        f0_cross_correlation_data_table];
+    fp = fullfile('data', subject_number, 'formants_cross_correlation_data_table');
+    fprintf(1, strcat('Writing file to ', fp))
+    save(fp, 'cross_correlation_data_table');
+    
+    function [cross_correlation_data_table] = save_corr(cross_correlation, epoch_order_pruned, formant, formants)
         % Index into correlations of specified formant
-        formant_table = find(constains(formants, formant));
+        formant_table = find(contains(formants, formant));
         cross_correlation = array2table(cross_correlation(:, :, formant_table));
         
+        % Create array for formants
+        formant_array(1:length(cross_correlation)) = formant;
+        
         % Add information to data table
-        cross_correlation_data_table = table([epoch_order_pruned.type],...
+        cross_correlation_data_table = table(formant_array,...
+            [epoch_order_pruned.type],...
             [epoch_order_pruned.epoch],...
             [epoch_order_pruned.word],...
             [cross_correlation],...
-            'VariableNames', {'condition', 'epoch', 'word', 'cross_correlation'});
-        
-        % Write data
-        fp = strcat(formant, '_cross_correlation_data_table'
-        save(fullfile('data', subject_number, 'cross_correlation_data_table'), 'cross_correlation_data_table')
+            'VariableNames', {'formant', 'condition', 'epoch', 'word', 'cross_correlation'});
     end
 
-    
-
-
-
+%     [y, Fs] = audioread('bruh.mp3')
+%     sound(y,Fs)
 
     %% Quit
     quit
