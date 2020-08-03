6850ec6f1080956f3d1ae7bd0061ca6336738d3b
letitia ho <letitiayhho@gmail.com>
2020-08-03 14:20:19 -0700

    Minor updates

--- src/get_mni_cortical_areas.R
+++ src/get_mni_cortical_areas.R
@@ -10,29 +10,36 @@ get_mni_cortical_areas <- function(radius = 5, save_file = NULL) {
   #'  (table) - table containing the original mni coordinates, aal.label, aal.distance,
   #'    ba.label, ba.distance of each channel
   
+  
   ## FUNCTIONS:
   get_coordinates <- function() {
     coordinates_fp <- "/Applications/eeglab2019/talker-change-data-processing/data/aggregate/mni_coordinates.txt"
     coordinates <- read.delim(coordinates_fp, header = FALSE, sep = "", dec = ".") %>%
+      subset(substr(V2, 1, 1) == "E") %>%
       select(V2, V3, V4, V5)
     colnames(coordinates) <- c("channels", "y", "x", "z")
     return(coordinates) }
   
+  
   ## SOURCE:
   library(dplyr)
   library(label4MRI)
-  source('/Applications/eeglab2019/talker-change-data-processing/src/get_mni_coordinates.R', echo=TRUE)
+  source('~/src/label4MRI/R/mni_to_radius_region_names.R')
 
+  
   ## MAIN:
   coordinates <- get_coordinates()
-  labels <- t(mapply(FUN = mni_to_region_name, x = coordinates$x, y = coordinates$y, z = coordinates$z))
+  # labels <- mapply(mni_to_radius_region_names, x = coordinates$x, y = coordinates$y, z = coordinates$z)
+  sphere_mnicoordiantes <- mapply(mni_to_radius_region_names, x = coordinates$x, y = coordinates$y, z = coordinates$z)
+
   
   ## SAVE:
-  coordinates = cbind(coordinates, labels)
-  if (!is.null(save_file)) {
-    save_file_fp = file.path("/Applications/eeglab2019/talker-change-data-processing/data/aggregate/", save_file)
-    write.table(format(coordinates, digits = 2), file = save_file_fp, append = FALSE, sep = "\t", col.names = TRUE, quote = FALSE)
-  }
+  # labeled_coordinates <- cbind(coordinates, labels)
+  # if (!is.null(save_file)) {
+  #   save_file_fp = file.path("/Applications/eeglab2019/talker-change-data-processing/data/aggregate/", save_file)
+  #   write.table(format(coordinates, digits = 2), file = save_file_fp, append = FALSE, sep = "\t", col.names = TRUE, quote = FALSE)
+  # }
   
-  return(coordinates)
+  
+  return(sphere_mnicoordiantes)
 }
