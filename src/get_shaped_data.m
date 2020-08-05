3ec8ecd09491d451a091b8bffe73ba746ff9c908
letitia ho <letitiayhho@gmail.com>
2020-08-05 10:05:31 -0700

    Works for formants

--- src/get_shaped_data.m
+++ src/get_shaped_data.m
@@ -4,11 +4,11 @@ function [] = get_shaped_data(method)
     %   stimuli across all subjects, channels and trials for each condition
     %
     % INPUT:
-    %   method - (char) 'cross_correlation' or 'convolution'
-    %   area - (char) 'anterior temporal', 'central temporal', 'premotor' or
-    %   'all'
+    %   method - (char) 'cross_correlation', 'convolution', 'RMS', or
+    %   'formant'
 
     %% Main
+    cd('/Applications/eeglab2019/talker-change-data-processing/')
     [data] = shape_data(method);
 
     % Export data
@@ -27,7 +27,11 @@ function [] = get_shaped_data(method)
             [subject_data, subject_number] = load_single_subject_data(method, i);
 
             % Calculate means for each channel for each condition
-            subject_means = grpstats(subject_data, 'condition');
+            if strcmp(method, 'formant') 
+                subject_means = grpstats(subject_data, {'condition', 'formant'});
+            else
+                subject_means = grpstats(subject_data, {'condition'});
+            end
 
             % Split conditions up
             conditions = subject_means.condition;
@@ -35,7 +39,7 @@ function [] = get_shaped_data(method)
 
             % Clean up the data table, add labels
             subject_means = removevars(subject_means, {'condition', 'GroupCount'}); 
-            subject_means.Properties.VariableNames = string(1:128);
+            subject_means.Properties.VariableNames = ['formant', string(1:128)];
             subject_means.Properties.RowNames = {};
 
             % Add condition codes to data table
@@ -50,76 +54,47 @@ function [] = get_shaped_data(method)
         end
     end
 
-    %% Conduct all pairwise t-tests
-    function [pairwise_h, pairwise_p, pairwise_t] = all_pairwise_t_tests(data)
-
-        % Preallocate memory
-        pairwise_h = zeros(3, 128);
-        pairwise_p = zeros(3, 128);
-        pairwise_t = zeros(3, 128);
-
-        % Run pairwise t-tests
-        conditions = {'constraint'; 'meaning'; 'talker'};
-        for i = 1:128
-            for j = 1:3
-                condition = conditions(j);
-                [h, p, ~, stats] = one_t_test(data, i, condition);
-                pairwise_h(j, i) = h;
-                pairwise_p(j, i) = p;
-                pairwise_t(j, i) = stats.tstat;
-            end
+    %% Load data of a single subject
+    function [subject_data, subject_number] = load_single_subject_data(method, i)
+        methods = {'cross_correlation', 'convolution', 'RMS', 'formant'};
+        if ~ismember(method, methods)
+            % Throw an error if incorrect method is specified
+            error('Invalid method, valid methods are ''convolution'','\...
+                '''cross_correlation'', ''RMS'', and ''formant''')
         end
 
-        % Add condition labels as row names
-        pairwise_h = array2table(pairwise_h, 'RowNames', conditions, 'VariableNames', string(1:128));
-        pairwise_p = array2table(pairwise_p, 'RowNames', conditions, 'VariableNames', string(1:128));
-        pairwise_t = array2table(pairwise_t, 'RowNames', conditions, 'VariableNames', string(1:128));
+        % Get name of the data files and their directory
+        file_names = strcat(method, '_data_table.mat');
+        data_files = dir(fullfile('data/**/', file_names));
+        data_table_full_path = fullfile(data_files(i).folder, file_names);
+
+        % Get subject number
+        subject_number = string(extractAfter(data_files(i).folder, 'data/'));
+
+        % Load data
+        subject_data = load(data_table_full_path);
+        subject_data = subject_data.('cross_correlation_data_table'); % CHANGE TO 'data_table' next time
+
+        % Convert into easily accessible form
+        subject_data = [subject_data.formant, subject_data.condition, subject_data.cross_correlation]; % CROSS CORRELATION? or just data
+        subject_data.Properties.VariableNames = ['formant', 'condition', string(1:128)];
     end
 
-    %% Other helper functions
+    %% Split four-letter condition code up into individual columns
+    function [split_conditions] = get_split_conditions(conditions)
+            % G/S: general (i.e. low) vs specific (i.e. high) constraint sentence stems
+            % M/N: meaningful vs nonsense ending word in the context of the rest of the sentence
+            % S/T: same vs different talker in the ending word
 
-        %% Load data of a single subject
-        function [subject_data, subject_number] = load_single_subject_data(method, i)
-            if ~strcmp(method, 'cross_correlation') &&...
-                ~strcmp(method, 'convolution') &&...
-                ~strcmp(method, 'RMS')
-                % Throw an error if incorrect method is specified
-                error('Invalid method, valid methods are ''convolution'','\...
-                    '''cross_correlation'', and ''RMS''')
-            end
-
-            % Get name of the data files and their directory
-            file_names = strcat(method, '_data_table.mat');
-            data_files = dir(fullfile('data/**/', file_names));
-            data_table_full_path = fullfile(data_files(i).folder, file_names);
-
-            % Get subject number
-            subject_number = string(extractAfter(data_files(i).folder, 'data/'));
-
-            % Load data
-            subject_data = load(data_table_full_path);
-            subject_data = subject_data.(strcat(method, '_data_table'));
-
-            % Convert into easily accessible form
-            subject_data = [subject_data.condition, subject_data.(method)];
-            subject_data.Properties.VariableNames = ['condition', string(1:128)];
+        for i = 1:size(conditions, 1)
+            condition = char(conditions(i, :));
+            constraint(i, :) = condition(1);
+            meaning(i, :) = condition(2);
+            talker(i, :) = condition(3);
         end
 
-        %% Split four-letter condition code up into individual arrays for each IV
-        function [split_conditions] = get_split_conditions(conditions)
-                % G/S: general (i.e. low) vs specific (i.e. high) constraint sentence stems
-                % M/N: meaningful vs nonsense ending word in the context of the rest of the sentence
-                % S/T: same vs different talker in the ending word
-
-            for i = 1:size(conditions, 1)
-                condition = char(conditions(i, :));
-                constraint(i, :) = condition(1);
-                meaning(i, :) = condition(2);
-                talker(i, :) = condition(3);
-            end
-
-            % Create table of separated IVs
-            split_conditions = table(constraint, meaning, talker);
-        end
+        % Create table of separated IVs
+        split_conditions = table(constraint, meaning, talker);
+    end
 
 end
