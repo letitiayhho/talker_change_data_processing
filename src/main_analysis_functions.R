5130f0740ec24f097de1a1df6a903de4185bad39
letitia ho <letitiayhho@gmail.com>
2020-09-22 11:43:02 -0400

    Separate function for getting frequency counts and function for generating the frequency table for display

--- src/main_analysis_functions.R
+++ src/main_analysis_functions.R
@@ -98,7 +98,7 @@ get_match <- function(group1, group2) {
 ## Formatting data tables for display
 
 # Get a single frequency table for results of the t-tests
-get_frequency_table <- function(data) {
+get_frequency_counts <- function(data) {
   hot <- get_hot(data)
   counts <- margin.table(table(hot), 1)
   proportions <- prop.table(table(hot))
@@ -106,10 +106,16 @@ get_frequency_table <- function(data) {
   else {return(data.frame("counts" = c(counts[1], counts[2]), "proportion" = c(proportions[1], proportions[2])))}
 }
 
+get_frequency_table <- function(xcorr, title = NULL) {
+  counts <- get_frequency_counts(xcorr)
+  kable(counts, caption = title) %>%
+    kable_styling(c("striped", "condensed"), full_width = F)
+}
+
 # Get a frequency table that combines results for xcorr and conv
 get_combined_frequency_table <- function(xcorr, conv, title = NULL) {
-  xcorr_freqs <- get_frequency_table(xcorr)
-  conv_freqs <- get_frequency_table(conv)
+  xcorr_freqs <- get_frequency_counts(xcorr)
+  conv_freqs <- get_frequency_counts(conv)
   kable(cbind(xcorr_freqs, conv_freqs), caption = title) %>%
     kable_styling(c("striped", "condensed"), full_width = F) %>%
     add_header_above(c(" ", "Cross-correlation" = 2, "Convolution" = 2))
@@ -117,9 +123,9 @@ get_combined_frequency_table <- function(xcorr, conv, title = NULL) {
 
 # Get a frequency table that combines results for the three formant levels
 get_formant_frequency_table <- function(f0, f1_f2, f3, title = NULL) {
-  f0_freqs <- get_frequency_table(f0)
-  f1_f2_freqs <- get_frequency_table(f1_f2)
-  f3_freqs <- get_frequency_table(f3)
+  f0_freqs <- get_frequency_counts(f0)
+  f1_f2_freqs <- get_frequency_counts(f1_f2)
+  f3_freqs <- get_frequency_counts(f3)
   kable(cbind(f0_freqs, f1_f2_freqs, f3_freqs), caption = title) %>%
     kable_styling(c("striped", "condensed"), full_width = F) %>%
     add_header_above(c(" ", "F0" = 2, "F1 + F2" = 2, "F3" = 2))
