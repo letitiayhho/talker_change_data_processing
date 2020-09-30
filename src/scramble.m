4ce2659f33e7be320214806828bde56d196b01fa
letitia ho <letitiayhho@gmail.com>
2020-09-30 13:01:45 -0400

    Initial commit

---
+++ src/scramble.m
@@ -0,0 +1,24 @@
+function [] = scramble(git_home, scrambles)
+arguments
+    git_home char
+    scrambles single
+end
+
+    %% Paths
+    cd(git_home)
+    load('scripts/subject_numbers.txt')
+    
+    %% Iterate
+    for i = 1:scrambles
+        
+        % Cross correlate each subject
+        for j = 1:11
+            subject_number = num2str(subject_numbers(j));
+            cross_correlate(git_home, subject_number, true)
+        end
+        
+        % Shape
+        shape_data(git_home, 'cross_correlations')
+        
+    end
+end
