b9fefb4909dc807beee96b1fb39dc45e0a7699fa
letitia ho <letitiayhho@gmail.com>
2020-09-29 22:00:21 -0400

    Compute averages across subject and condition levels within this script

--- src/shape_data.m
+++ src/shape_data.m
@@ -9,7 +9,7 @@ function [] = shape_data(git_home, file_name)
 
     %% Main
     cd(git_home)
-    statistics = {'abs_average', 'lag', 'maximum'};
+    statistics = {'abs_average', 'maximum', 'lag'};
     shape_all(file_name, statistics)
         
     %% Call shape data on each statistic
@@ -17,7 +17,7 @@ function [] = shape_data(git_home, file_name)
         for i = 1:length(statistics)
             statistic = statistics{i};
             [data] = shape_data(file_name, statistic);
-            writetable(data, strcat('data/aggregate/', file_name, '_', statistic, '.csv'));
+            writetable(data, strcat('data/aggregate/', file_name, '_', statistic, '_test.csv'));
         end
     end
 
@@ -31,49 +31,57 @@ function [] = shape_data(git_home, file_name)
 
         % Iterate over subjects
         for i = 1:number_of_subjects
-            [subject_data, subject_number] = load_single_subject_data(file_name, statistic, i);
-
-            % Calculate means for each channel for each condition
-            subject_means = grpstats(subject_data, {'condition'});
+            [subject_number, conditions, cross_correlations] = load_single_subject_data(file_name, statistic, i);
 
             % Split conditions up
-            conditions = subject_means.condition;
             split_conditions = get_split_conditions(conditions);
 
-            % Clean up the data table, add labels
-            subject_means = removevars(subject_means, {'condition', 'GroupCount'}); 
-            subject_means.Properties.VariableNames = string(1:128);
-            subject_means.Properties.RowNames = {};
-
             % Add condition codes to data table
-            subject_means = [split_conditions, subject_means];
+            cross_correlations = [split_conditions, cross_correlations];
 
             % Add subject number to data table
-            subject_means.subject_number(:) = subject_number;
-            subject_means = movevars(subject_means, 'subject_number', 'Before', 'constraint');
+            cross_correlations.subject(:) = subject_number;
+            cross_correlations = movevars(cross_correlations, 'subject', 'Before', 'constraint');
+            
+            % Calculate means for each channel for each condition
+            talker = grpstats(removevars(cross_correlations, {'meaning', 'constraint'}), {'subject', 'talker'});
+            meaning = grpstats(removevars(cross_correlations, {'talker', 'constraint'}), {'subject', 'meaning'});
+            constraint = grpstats(removevars(cross_correlations, {'talker', 'meaning'}), {'subject', 'constraint'});
+            
+            % Clean up the data table, add labels
+            talker = removevars(talker, {'GroupCount'}); 
+            meaning = removevars(meaning, {'GroupCount'});
+            constraint = removevars(constraint, {'GroupCount'}); 
+            
+            % Change variable names
+            talker.Properties.VariableNames = ['subject', 'condition', string(1:128)];
+            talker.Properties.RowNames = {};
+            meaning.Properties.VariableNames = ['subject', 'condition', string(1:128)];
+            meaning.Properties.RowNames = {};
+            constraint.Properties.VariableNames = ['subject', 'condition', string(1:128)];
+            constraint.Properties.RowNames = {};
 
-            % Combine all subjects into one table
-            data = [data; subject_means];
+            % Combine all subjects into one tabled
+            data = [data; talker; meaning; constraint];
         end
     end
 
     %% Load data of a single subject
-    function [cross_correlations, subject_number] = load_single_subject_data(file_name, statistic, i)
+%     function [cross_correlations, subject_number] = load_single_subject_data(file_name, statistic, i)
+    function [subject_number, conditions, cross_correlations] = load_single_subject_data(file_name, statistic, i)
 
         % Get name of the data files and their directory
         file_names = strcat(file_name, '.mat');
         data_files = dir(fullfile('data/**/', file_names));
         data_file_full_path = fullfile(data_files(i).folder, file_names);
 
-        % Get subject number
-        subject_number = string(extractAfter(data_files(i).folder, 'data/'));
-
         % Load data
         load(data_file_full_path);
 
-        % Convert into easily accessible form
-        cross_correlations = cell2table([cross_correlations.condition, num2cell(cross_correlations.(statistic))]);
-        cross_correlations.Properties.VariableNames = ['condition', string(1:128)];
+        % Bind to variables for returning
+        subject_number = string(extractAfter(data_files(i).folder, 'data/'));
+        conditions = cross_correlations.condition;
+        cross_correlations = cell2table(num2cell(cross_correlations.(statistic)));
     end
 
     %% Split four-letter condition code up into individual columns
@@ -84,7 +92,12 @@ function [] = shape_data(git_home, file_name)
 
         for i = 1:size(conditions, 1)
             condition = char(conditions(i, :));
-            constraint(i, :) = condition(1);
+            % Recode S to H to avoid clash with same talker condition
+            if condition(1) == 'S'
+                constraint(i, :) = 'H';
+            else
+                constraint(i, :) = 'L';
+            end
             meaning(i, :) = condition(2);
             talker(i, :) = condition(3);
         end
