183ccecabdbab533580f853f180fc0d0cb2ccd1d
letitia ho <letitiayhho@gmail.com>
2020-11-04 14:17:13 -0500

    Rename resample to "shuffle", added argument validation

--- src/shape_shuffles.m
+++ src/shape_shuffles.m
@@ -1,29 +1,33 @@
 function[] = shape_shuffles(statistic)
 %% Shape_shuffles.m
 % DESCRIPTION:
-%     Takes resampled cross correlation averages or maximums and
+%     Takes shuffled cross correlation averages or maximums and
 %     concatenates them all into a .csv file
 %
 % OUTPUT:
 %     Writes files shuffed_<statistic>.csv and <statistic>.csv
 
+arguments
+    statistic char = 'maximum'
+end
+
 %% Main
 % get file names
-resampled_files = get_file_names('data/aggregate/shuffles/', statistic);
+shuffled_files = get_file_names('data/aggregate/shuffles/', statistic);
 original_files = get_file_names('data/aggregate/', statistic);
 
 % average across original data
 original_averages = average_across_conditions(original_files);
 
-% average across all resampled data and combine into one table
-resampled_averages = get_resampled_averages(resampled_files);
+% average across all shuffled data and combine into one table
+shuffled_averages = get_shuffled_averages(shuffled_files);
 
 % write to spreadsheet
 writetable(original_averages, strcat('data/aggregate/', statistic, '.csv'))
-writetable(resampled_averages, strcat('data/aggregate/shuffled_', statistic, '.csv'))
+writetable(shuffled_averages, strcat('data/aggregate/shuffled_', statistic, '.csv'))
 
 %% Functions
-% function to get all resampled data file_names
+% function to get all shuffled data file_names
     function [full_file_names] = get_file_names(path, statistic)
         dir_details = what(path);
         all_mat_files = dir_details.mat;
@@ -32,14 +36,14 @@ writetable(resampled_averages, strcat('data/aggregate/shuffled_', statistic, '.c
     end
 
 % function to load data
-    function [resampled_averages] = get_resampled_averages(file_names)
-        resampled_averages = [];
+    function [shuffled_averages] = get_shuffled_averages(file_names)
+        shuffled_averages = [];
         for i = 1:length(file_names)
             averages = average_across_conditions(file_names(i));
-            resample_number = repmat(i, 6, 1);
-            averages.Properties.RowNames = string(resample_number) + averages.Properties.RowNames;
-            averages = addvars(averages, resample_number, 'Before', 'condition', 'NewVariableNames', 'resample');
-            resampled_averages = [resampled_averages; averages];
+            shuffle_number = repmat(i, 6, 1);
+            averages.Properties.RowNames = string(shuffle_number) + averages.Properties.RowNames;
+            averages = addvars(averages, shuffle_number, 'Before', 'condition', 'NewVariableNames', 'shuffle_number');
+            shuffled_averages = [shuffled_averages; averages];
         end
     end
 
