0ccbcc31a8f90d9cd3b9eaea8e77ac48c1f92983
letitia ho <letitiayhho@gmail.com>
2021-01-21 16:14:55 -0800

    Clean up defunct scripts, reinstate src/get_rms.m

---
+++ src/get_rms.m
@@ -0,0 +1,85 @@
+function[] = get_rms(subject_number)
+    fprintf(1, strcat('Analyzing data from subject #', subject_number, '\n'))
+
+    %% 1. Import data
+    cd('/Applications/eeglab2019/talker-change-data-processing')
+    addpath(fullfile('data', subject_number)) % add subject data to path
+    addpath(fullfile('data/stim')) % add audio stimuli directory to path
+    
+    % Import EEG data
+    eeg_data = load('eeg_data');
+    eeg_data = eeg_data.('eeg_data');
+
+    % Import original epoch order
+    epoch_order_original = load('epoch_order_original');
+    epoch_order_original = epoch_order_original.('epoch_order_original');
+
+    % Import pruned epoch order
+    epoch_order_pruned = load('epoch_order_pruned');
+    epoch_order_pruned = epoch_order_pruned.('epoch_order_pruned');
+
+    % Import stimuli order
+    stim_order = readtable('stim_order.txt');
+
+    %% 2. Match EEG epochs with words
+    % Sort original epoch order by condition
+    epoch_order_original = struct2table(epoch_order_original);
+    epoch_order_original = sortrows(epoch_order_original, 'type');
+    epoch_order_original = epoch_order_original(endsWith(epoch_order_original.type, 'E'),:);
+
+    % Sort pruned epoch order by condition
+    epoch_order_pruned = struct2table(epoch_order_pruned);
+    epoch_order_pruned = sortrows(epoch_order_pruned, 'type');
+
+    % Match pruned epochs with corresponding epochs
+    j = 1;
+    for i = 1:height(epoch_order_original)
+        % Match original epochs with corresponding stim 
+        epoch_order_original.word(i) = stim_order.ending(i);
+        
+        % Break at the end of pruned epochs to avoid exceeding array length
+        if j > height(epoch_order_pruned)
+            break
+        end
+        
+        % Match pruned epochs with corresponding stim
+        if epoch_order_original.urevent(i) == epoch_order_pruned.urevent(j)
+            epoch_order_pruned.word(j) = stim_order.ending(i);
+            j = j+1;
+        end
+    end
+
+    % Sort pruned epoch order by latency
+    epoch_order_pruned = sortrows(epoch_order_pruned, 'latency');
+
+    %% 3. Compute RMS
+    % Initialize data table
+    rms = zeros(size(eeg_data, 3), size(eeg_data, 1));
+
+    % Loop over channels
+    for i = 1:size(eeg_data, 1)
+        disp(strcat('Channel #', num2str(i)))
+
+        % Loop over epochs
+         for j = 1:size(eeg_data, 3)
+             
+             % Compute RMS
+             epoch = eeg_data(i, :, j);
+             rms(j, i) = rms(epoch);
+         end
+    end
+
+    %% 4. Write data
+    rms = array2table(rms);
+    rms_data_table = table([epoch_order_pruned.type],...
+        [epoch_order_pruned.epoch],...
+        [epoch_order_pruned.word],...
+        [rms],...
+        'VariableNames', {'condition', 'epoch', 'word', 'rms'});
+    
+    % Write data
+    save(fullfile('data', subject_number, 'rms_data_table'), 'rms_data_table')
+    
+    %% Quit
+    quit
+end
