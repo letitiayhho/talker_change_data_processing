f68553f91ce10179a43e0195e44e0c9cb619887d
letitia ho <letitiayhho@gmail.com>
2021-06-21 15:34:23 -0700

    Start t-test based analyses

---
+++ 8_t_tests/src/get_xcorr_for_all_subjects.m
@@ -0,0 +1,28 @@
+function [] = concat_cross_correlations(git_home)
+% DESCRIPTION:
+%   Computes cross-correlations or convolutions between eeg signal and audio
+%   stimuli across all subjects, channels and trials for each condition
+
+arguments
+    git_home char
+end
+
+    %% Main
+    cd(git_home)    
+    addpath('src/tools/')
+    file_struct = dir(fullfile('data/2_cross_correlations/*/cross_correlations.mat'));
+    data = combine_cross_correlations(file_struct);
+    writetable(data, 'data/5_rms/maximum.csv')
+    quit
+        
+    %% Combine data file from each subject
+    function [data] = combine_cross_correlations(file_struct)
+        data = table();
+        for i = 1:length(file_struct)
+            path = fullfile(file_struct(i).folder, 'cross_correlations.mat');
+            cross_correlations = load(path).cross_correlations;
+            data = [data; cross_correlations];
+        end
+    end
+
+end
