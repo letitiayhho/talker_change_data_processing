f7eebc98270d6571c582b07ec02bb004875bc05c
letitia ho <letitiayhho@gmail.com>
2021-06-23 14:32:54 -0700

    Clean up and simplify

--- 8_t_tests/src/concat_xcorr.m
+++ 8_t_tests/src/concat_xcorr.m
@@ -1,28 +1,30 @@
-function [] = concat_xcorr(git_home)
+function [] = concat_xcorr(git_home, stat)
 % DESCRIPTION:
 %   Computes cross-correlations or convolutions between eeg signal and audio
 %   stimuli across all subjects, channels and trials for each condition
 
 arguments
     git_home char
+    stat char % 'maximum', 'lag' or 'abs_average'
 end
 
     %% Main
     cd(git_home)    
     addpath('tools/')
-    file_struct = dir(fullfile('2_cross_correlations/data/*/cross_correlations.mat'));
+    file_struct = dir(['2_cross_correlate/data/*/', stat, '.mat']);
     data = combine_cross_correlations(file_struct);
-    writetable(data, '8_t_tests/data/maximum.csv')
+    write_fp = ['8_t_tests/data/', stat, '.csv'];
+    fprintf(1, ['Writing data to ', write_fp, '\n']);
+    writetable(data, write_fp)
     quit
         
     %% Combine data file from each subject
     function [data] = combine_cross_correlations(file_struct)
         data = table();
         for i = 1:length(file_struct)
-            path = fullfile(file_struct(i).folder, 'cross_correlations.mat');
-            cross_correlations = load(path).cross_correlations;
+            path = fullfile(file_struct(i).folder, file_struct(i).name);
+            cross_correlations = load(path).data_frame;
             data = [data; cross_correlations];
         end
     end
-
 end
