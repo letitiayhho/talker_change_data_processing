ba0c5e2ac720a58c692ba51c473cb51a48ba76a8
letitia ho <letitiayhho@gmail.com>
2021-06-24 12:12:26 -0700

    Create table with p-values colored by significance

--- 8_t_tests/src/t_test_functions.R
+++ 8_t_tests/src/t_test_functions.R
@@ -24,3 +24,37 @@ get_one_sample_t <- function(data) {
   p <- apply(data, MARGIN = 2, function(channel) {t.test(channel)$p.value})
   return(data.frame("t" = t, "df" = df, "p" = p))
 }
+
+# Taking the p-values of the t-test results and assigning a 1 if sig, 0 if not
+get_hot <- function(data, alpha = 0.05) {
+  hot <- c()
+  hot[data$p < alpha] <- 1
+  hot[data$p >= alpha] <- 0
+  return(hot)
+}
+
+## Formatting data tables for display
+
+# Get a single frequency table for results of the t-tests
+get_frequency_counts <- function(data) {
+  hot <- get_hot(data)
+  counts <- margin.table(table(hot), 1)
+  proportions <- prop.table(table(hot))
+  if (dim(counts) == 1) {return(data.frame("counts" = c(counts[1], 0), "proportion" = c(proportions[1], 0)))}
+  else {return(data.frame("counts" = c(counts[1], counts[2]), "proportion" = c(proportions[1], proportions[2])))}
+}
+
+get_frequency_table <- function(xcorr, title = NULL) {
+  counts <- get_frequency_counts(xcorr)
+  kable(counts, caption = title) %>%
+    kable_styling(c("striped", "condensed"), full_width = F)
+}
+
+# Get a frequency table that combines results for xcorr and conv
+get_combined_frequency_table <- function(xcorr, conv, title = NULL) {
+  xcorr_freqs <- get_frequency_counts(xcorr)
+  conv_freqs <- get_frequency_counts(conv)
+  table <- kable(cbind(xcorr_freqs, conv_freqs), caption = title) %>%
+    kable_styling(c("striped", "condensed"), full_width = F)
+  return(table)
+}
