4e204b9418242a1f202393261ede28c65f39aec1
letitia ho <letitiayhho@gmail.com>
2021-07-06 14:29:22 -0700

    Run two-sample t-tests and plot lags

--- 8_t_tests/src/t_test_functions.R
+++ 8_t_tests/src/t_test_functions.R
@@ -17,10 +17,25 @@ subset <- function(data, formants = NaN, condition, level) {
   }
 }
 
-# Function for running a one-sample t-test and saving the p-value, t-statistic, and df
 get_one_sample_t <- function(data) {
   t <- apply(data, MARGIN = 2, function(channel) {t.test(channel)$statistic})
   df <- apply(data, MARGIN = 2, function(channel) {t.test(channel)$parameter})
   p <- apply(data, MARGIN = 2, function(channel) {t.test(channel)$p.value})
   return(data.frame("t" = t, "df" = df, "p" = p))
+}
+
+get_two_sample_t <- function(group1, group2) {
+  t <- mapply(function(x, y) {t.test(x, y)$statistic}, group1, group2)
+  df <- mapply(function(x, y) {t.test(x, y)$parameter}, group1, group2)
+  p <- mapply(function(x, y) {t.test(x, y)$p.value}, group1, group2)
+  return(data.frame("t" = t, "df" = df, "p" = p))
+}
+
+get_lag_distribution <- function(group1, group2, labels, channel) {
+  labels <- c(rep(labels[1], nrow(group1)), rep(labels[2], nrow(group2)))
+  data <- data.frame(labels, lags = c(group1[[paste("X", channel, sep = "")]], group2[[paste("X", channel, sep = "")]]))
+  plot <- ggplot(data = data, aes(lags, colour = labels)) +
+    geom_density() +
+    ggtitle(paste("Channel", channel))
+  return(plot)
 }
