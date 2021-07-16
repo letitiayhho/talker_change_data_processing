861cbc2b035cc9e383c52f0e8316dcb6101d7a6c
letitia ho <letitiayhho@gmail.com>
2021-07-16 11:09:24 -0500

    Run script over all conditions

--- 9_N1_RMS/src/average_over_epochs.m
+++ 9_N1_RMS/src/average_over_epochs.m
@@ -1,24 +1,44 @@
-function [] = average_over_epochs(git_home, condition)
+% function [] = average_over_epochs(git_home, talker, meaning, constraint)
+%     arguments
+%         git_home char
+%         talker char {mustBeMember(talker,["same","different","NaN"])} = 'NaN'
+%         meaning char {mustBeMember(meaning,["meaningful","nonsense","NaN"])}= 'NaN'
+%         constraint char {mustBeMember(constraint,["low","high","NaN"])} = 'NaN'
+%         end
+%     
+% cd(git_home)
+% subject_numbers = load_subject_numbers('./0_set_up_and_raw_data/data/subject_numbers.txt');
+% 
+% for i = 1:length(subject_numbers)
+%     subject_number = num2str(subject_numbers(i));
+subject_number = '301';
 
-arguments
-    git_home string
-    condition string % 'same', 'different', 'meaningful', 'nonsense', 'low', 'high'
-end
-
-cd('git_home')
-subject_numbers = load_subject_numbers('./0_set_up_and_raw_data/data/subject_numbers.txt');
-
-for i = 1:length(subject_numbers)
-    subject_number = num2str(subject_numbers(i));
     fprintf(1, ['Averaging over epochs for subject ', subject_number, '\n'])
     conditions = load_conditions(subject_number);
     eeg = load_eeg(subject_number);
-    eeg = subset_by_condition(eeg, conditions, condition);
-    eeg = flatten(eeg);
-    savefp = fullfile('./9_N1_RMS/data', subject_number, ['eeg_data_', condition, '.csv']);
+    all_conditions_eeg = average_over_conditions(eeg, conditions);
+    savefp = fullfile('./9_N1_RMS/data', subject_number, 'eeg_data_averaged.csv');
     fprintf(1, ['Saving data to ', savefp, '\n'])
-    writematrix(eeg, savefp)
-end
+    writetable(all_conditions_eeg, savefp)
+% end
+
+% quit
+
+    function [all_conditions_eeg] = average_over_conditions(eeg, conditions)
+        all_conditions_eeg = [];
+        for talker = ["S", "T"] % double quotes matters! needs to be string not char
+            for meaning = ["M", "N"]
+                for constraint = ["L", "H"]
+                    one_condition_eeg = subset_by_condition(eeg, conditions, talker, meaning, constraint);
+                    one_condition_eeg = array2table(flatten(one_condition_eeg));
+                    conditions = table(repmat(talker, 1, 128)', repmat(meaning, 1, 128)', repmat(constraint, 1, 128)', (1:128)');
+                    conditions.Properties.VariableNames(1:4) = {'talker', 'meaning', 'constraint', 'channel'};
+                    one_condition_eeg = [conditions, one_condition_eeg];
+                    all_conditions_eeg = [all_conditions_eeg; one_condition_eeg];
+                end
+            end
+        end
+    end
 
     function [subject_numbers] = load_subject_numbers(fp)
         fileID = fopen(fp,'r');
@@ -32,9 +52,8 @@ end
 
     function [conditions] = load_conditions(subject_number)
         subject_dir = fullfile('./2_cross_correlate/data',  subject_number);
-        filepath = fullfile(subject_dir, 'stim_order.mat');
-        conditions = load(filepath).stim_order;
-        conditions = cell2mat(conditions.type);
+        filepath = fullfile(subject_dir, 'split_conditions.mat');
+        conditions = load(filepath).split_conditions;
     end
 
     function [eeg] = add_subject_number_column(subject_number, eeg)
@@ -42,33 +61,37 @@ end
         eeg = [subject_number_column', eeg];
     end
 
-    function [eeg] = subset_by_condition(eeg, conditions, condition)
-        indexes = get_condition_indexes(conditions, condition);
+    function [eeg] = subset_by_condition(eeg, conditions, talker, meaning, constraint)
+        talker_indexes = 1:size(conditions,1);
+        meaning_indexes = 1:size(conditions, 1);
+        constraint_indexes = 1:size(conditions, 1);
+            
+%         if strcmp(talker, 'same')
+            talker_indexes = find(contains(conditions.talker, talker));
+%         elseif strcmp(talker, 'different')
+%             talker_indexes = find(contains(conditions.talker, 'T'));
+%         end
+        
+%         if strcmp(meaning, 'meaningful')
+            meaning_indexes = find(contains(conditions.meaning, meaning));
+%         elseif strcmp(meaning, 'nonsense')
+%             meaning_indexes = find(contains(conditions.meaning, 'N'));
+%         end
+        
+%         if strcmp(constraint, 'low')
+            constraint_indexes = find(contains(conditions.constraint, constraint));
+%         elseif strcmp(constraint, 'high')
+%             constraint_indexes = find(contains(conditions.constraint, 'H'));
+%         end
+        
+        % Intersect of all indexes
+        indexes = intersect(intersect(talker_indexes, meaning_indexes), constraint_indexes);
         eeg = eeg(:, :, indexes);
     end
 
-    function [indexes] = get_condition_indexes(conditions, condition)
-        constraint_codes = string(conditions(:, 1));
-        meaning_codes = string(conditions(:, 2));
-        talker_codes = string(conditions(:, 3));
-        if strcmp(condition, 'same')
-            indexes = find(contains(talker_codes, 'S'));
-        elseif strcmp(condition, 'different')
-            indexes = find(contains(talker_codes, 'T'));
-        elseif strcmp(condition, 'meaningful')
-            indexes = find(contains(meaning_codes, 'M'));
-        elseif strcmp(condition, 'nonsense')
-            indexes = find(contains(meaning_codes, 'N'));
-        elseif strcmp(condition, 'low')
-            indexes = find(contains(constraint_codes, 'G'));
-        elseif strcmp(condition, 'high')
-            indexes = find(contains(constraint_codes, 'S'));
-        end
-    end
-
     function [eeg] = flatten(eeg)
-        eeg = mean(eeg, 3);
+        eeg = mean(eeg, 3); % data will be 128*1600
     end
 
-end
+% end
 
