1757daa2600e47d942440ad346ca002bf8052d4c
letitia ho <letitiayhho@gmail.com>
2021-08-15 21:03:56 -0500

    Clean up script to match chunk order with .ipynb version

--- 7_coherence/src/test_coherence.m
+++ 7_coherence/src/test_coherence.m
@@ -16,29 +16,14 @@ stim = resample(stim, 10, 441);
 pad = zeros(length(eeg) - length(stim), 1);
 stim = [stim; pad];
 
-%% Vectorized coherence
-% eeg = double(eeg_data(:, :, epoch_num))';
-% [cxy, f] = mscohere(stim, eeg, [], [], [], fs);
-% cxy = mean(cxy, 2);
-% plot(f, cxy)
-% title("Coherence between stim and eeg: all channels")
-% xlabel("Frequency")
-
 %% Plot power spectra
-% plot_power_spectra(stim, fs)
-% plot_power_spectra(eeg, fs)
+plot_power_spectra(stim, fs)
+plot_power_spectra(eeg, fs)
 
 %% Plot coherence
-% plot_coherence(stim, eeg, fs)
-% lowpass_stim = lowpass(stim, 100, fs);
-% lowpass_eeg = lowpass(eeg, 100, fs);
-% plot_coherence(lowpass_stim, lowpass_eeg, fs)
+plot_coherence(stim, eeg, fs)
 
-%% Pitch
-winLength = round(.05*fs);
-f0 = pitch(stim, fs, 'WindowLength',winLength);
-plot(f0)
-
+%% FUNCTIONS
 % Power spectra
 function [] = plot_power_spectra(x, fs)
     y = fft(x);
@@ -62,34 +47,15 @@ function [] = plot_coherence(x, y, fs)
     xlabel("Frequency")
 end
 
-% figure
-% lowpass_stim = lowpass(stim, 100, fs);
-% lowpass_eeg = lowpass(eeg, 100, fs);
-% [cxy, f] = mscohere(lowpass_stim, lowpass_eeg, [], [], [], fs);
+%% Compute pitch
+% winLength = round(.05*fs);
+% f0 = pitch(stim, fs, 'WindowLength',winLength);
+% plot(f0)
+
+%% Test vectorized coherence
+% eeg = double(eeg_data(:, :, epoch_num))';
+% [cxy, f] = mscohere(stim, eeg, [], [], [], fs);
+% cxy = mean(cxy, 2);
 % plot(f, cxy)
-% title("Coherence between low-pass filtered stim and eeg")
+% title("Coherence between stim and eeg: all channels")
 % xlabel("Frequency")
-
-% Vectorized implementation (nrows = time points)
-% [cross_correlation, lag] = xcorr(epoch, stim);
-% cross_correlations = [cross_correlations; cross_correlation];
-% abs_average = [abs_average, mean(abs(cross_correlation))];
-% [m, I] = max(abs(cross_correlation));
-% maximum = [maximum, m];
-% lags = [lag(I)];
-% end
-
-% time_points = -100:1499;
-% figure
-% plot(time_points, epoch)
-% xlim([-100, 1500])
-% ylim([-20, 20])
-% figure
-% plot(stim)
-% xlim([-100, 1500])
-% ylim([-1, 1])
-% figure
-% plot(cross_correlation)
-% xlim([0, 3200])
-% cross_correlation = cross_correlation/max(cross_correlation);
-% audiowrite('sample_cross_correlation.wav', cross_correlation, 1000)
