6c6a4a94f7d39a3934edf29da5486c1e2a540c49
letitia ho <letitiayhho@gmail.com>
2021-12-10 15:54:12 -0600

    Move all existing eeg_data.mat into the EEG.data file in the 1_set_up_and_raw_data/*/eeg_data.set files

--- 1_preprocessing/src/preprocess_all_eeg_data.m
+++ 1_preprocessing/src/preprocess_all_eeg_data.m
@@ -1,5 +1,5 @@
 function [] = preprocess_all_eeg_data()
-subjects = {'301';'302';'303';'304';'305';'306';'307';'308';'310';'315';'316';'317'};
+subjects = ["301", "302", "303", "304", "305", "307", "308", "310", "315", "316", "317"];
     for subject = subjects
         preprocess_all_eeg_data(subject)
     end
