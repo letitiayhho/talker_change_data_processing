9a8d3f3e7dfd53601f5d140607d8b822d46b6968
letitia ho <letitiayhho@gmail.com>
2022-03-22 15:02:25 -0500

    Add permutation test for coherence across all conditions, overall

--- 7_cluster_coherence/src/get_weight_scores.R
+++ 7_cluster_coherence/src/get_weight_scores.R
@@ -7,14 +7,15 @@ library("ggpubr")
 source("tools/functions.R")
 source("7_cluster_coherence/src/functions.R")
 
-coherr <- read.csv(file = "7_coherence/data/average.csv")
+coherr <- read.csv(file = "6_coherence/data/average.csv")
+overall <- subset(coherr, keepSubjNum = FALSE) %>% colMeans() %>% as.double() %>% normalize()
 S <- subset(coherr, talker = "S", keepSubjNum = FALSE) %>% colMeans() %>% as.double() %>% normalize()
 T <- subset(coherr, talker = "T", keepSubjNum = FALSE) %>% colMeans() %>% as.double() %>% normalize()
 M <- subset(coherr, meaning = "M", keepSubjNum = FALSE) %>% colMeans() %>% as.double() %>% normalize()
 N <- subset(coherr, meaning = "N", keepSubjNum = FALSE) %>% colMeans() %>% as.double() %>% normalize()
 L <- subset(coherr, constraint = "L", keepSubjNum = FALSE) %>% colMeans() %>% as.double() %>% normalize()
 H <- subset(coherr, constraint = "H", keepSubjNum = FALSE) %>% colMeans() %>% as.double() %>% normalize()
-weight_scores <- list("S" = S, "T" = T, "M" = M, "N" = N, "L" = L, "H" = H)
+weight_scores <- list("overall" = overall, "S" = S, "T" = T, "M" = M, "N" = N, "L" = L, "H" = H)
 
 # Save variables
 save_filepath <- paste("7_cluster_coherence/data/weight_scores/average.RDS", sep = "")
