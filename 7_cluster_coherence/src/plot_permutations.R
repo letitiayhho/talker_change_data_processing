77d7b5e421fd8706c8529f60343b7352fa6787e4
letitia ho <letitiayhho@gmail.com>
2022-03-22 14:55:33 -0500

    Write script to plot the permutation test results

---
+++ 7_cluster_coherence/src/plot_permutations.R
@@ -0,0 +1,37 @@
+#!/usr/bin/env Rscript
+
+library("dplyr")
+library("ggplot2")
+library("ggpubr")
+source("tools/functions.R")
+source("7_cluster_coherence/src/functions.R")
+
+# Get command line args
+args = commandArgs(trailingOnly=TRUE)
+if (length(args) != 1) {
+  stop("One argument ('S', 'T', 'talker', 'meaning', etc) must be supplied", call.=FALSE)
+} 
+condition = args[1]
+
+# Load distance scores
+distance_scores <- readRDS("7_cluster_coherence/data/distance_scores/distance_scores.RDS")
+
+# Load weight scores
+filepath <- paste("7_cluster_coherence/data/weight_scores/average.RDS", sep = "")
+all_weight_scores <- readRDS(filepath)
+weight_scores <- all_weight_scores[[condition]]
+
+# Compute observed cluster scores with distance and weight scores
+observed_filename <- paste("7_cluster_coherence/data/cluster_scores/", condition, "_observed.RDS", sep = "")
+observed <- readRDS(observed_filename)
+
+# Compute cluster scores with permuted weight scores
+permutations_filename <- paste("7_cluster_coherence/data/cluster_scores/", condition, "_permutations.RDS", sep = "")
+permutations <- readRDS(permutations_filename)
+
+# Plot permutation test results
+hist_plot <- histogram(permutations$sum, observed$sum, title = condition)
+
+# Save data and figures
+fig_filename <- paste("7_cluster_coherence/figs/", condition, ".png", sep = "")
+ggsave(hist_plot, filename = fig_filename, width = 8, height = 6)
