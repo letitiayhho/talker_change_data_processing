68e79f0f7c6bb73730ef4e46adcabbc2c957a784
letitia ho <letitiayhho@gmail.com>
2022-09-22 11:38:43 -0500

    Run analysis on normalized xcorr, make exploratory plots

--- 4_test_cross_correlations/src/functions.R
+++ 4_test_cross_correlations/src/functions.R
@@ -25,6 +25,43 @@ subset <- function(data, talker = NaN, meaning = NaN, constraint = NaN, keepLabe
   return(data)
 }
 
+plot_average_xcorrs <- function(condition_xcorrs, fname, p) { # should be n_subs x n_chans 
+  means <- c(colMeans(as.matrix(condition_xcorrs)))
+  sds <- c(sapply(condition_xcorrs, sd))
+  # p <- (1/p)/max(1/p) 
+  
+  # order by inverse p-value size
+  means <- means[order(p)]
+  sds <- sds[order(p)]
+  p <- p[order(p)]
+  df <- data.frame(chans <- c(1:128), means, sds, p)
+  
+  plot <- ggplot(df, aes(x = chans, y = means)) +
+    geom_point() +
+    geom_errorbar(aes(ymin = means - sds, ymax = means + sds)) +
+    ylab('Average cross correlation') +
+    xlab('Channel') +
+    ylim(-1, 1) + 
+    geom_point(aes(x = chans, y = p))
+  ggsave(plot = plot, filename = fname, width = 6, height = 3.5)
+  return(plot)
+}
+
+get_one_sample_subject_wilcoxon <- function(condition_xcorrs) {
+  sub_means <- c(rowMeans(as.matrix(condition_xcorrs)))
+  w <- wilcox.test(sub_means)
+  return(list('w' = w$statistic,
+              'p' = w$p.value))
+}
+
+get_two_sample_subject_wilcoxon <- function(condition1_xcorrs, condition2_xcorrs) {
+  cond1_sub_means <- c(rowMeans(as.matrix(condition1_xcorrs)))
+  cond2_sub_means <- c(rowMeans(as.matrix(condition2_xcorrs)))
+  w <- wilcox.test(cond1_sub_means, y = cond2_sub_means)
+  return(list('w' = w$statistic,
+              'p' = w$p.value))
+}
+
 get_subject_averages <- function(data) {
   channel_labels <- paste("X", as.character(1:128), sep = "")
   data <-  data %>%
@@ -34,27 +71,27 @@ get_subject_averages <- function(data) {
   return(data)
 }
 
-get_one_sample_t <- function(data) {
+get_one_sample_t_for_each_channel <- function(data) {
   t <- apply(data, MARGIN = 2, function(channel) {t.test(channel)$statistic})
   df <- apply(data, MARGIN = 2, function(channel) {t.test(channel)$parameter})
   p <- apply(data, MARGIN = 2, function(channel) {t.test(channel)$p.value})
   return(data.frame("t" = t, "df" = df, "p" = p))
 }
 
-get_one_sample_wilcoxon <- function(data, alt) {
+get_one_sample_wilcoxon_for_each_channel <- function(data, alt) {
   w <- apply(data, MARGIN = 2, function(channel) {wilcox.test(channel, exact = TRUE, alternative = c(alt))$statistic})
   p <- apply(data, MARGIN = 2, function(channel) {wilcox.test(channel, exact = TRUE, alternative = c(alt))$p.value})
   return(data.frame("w" = w, "p" = p))
 }
 
-get_two_sample_t <- function(group1, group2) {
+get_two_sample_t_for_each_channel <- function(group1, group2) {
   t <- mapply(function(x, y) {t.test(x, y)$statistic}, group1, group2)
   df <- mapply(function(x, y) {t.test(x, y)$parameter}, group1, group2)
   p <- mapply(function(x, y) {t.test(x, y)$p.value}, group1, group2)
   return(data.frame("t" = t, "df" = df, "p" = p))
 }
 
-get_two_sample_wilcoxon <- function(group1, group2) {
+get_two_sample_wilcoxon_for_each_channel <- function(group1, group2) {
   w <- mapply(function(x, y) {wilcox.test(x, y, paired = TRUE, exact = TRUE)$statistic}, group1, group2)
   p <- mapply(function(x, y) {wilcox.test(x, y, paired = TRUE, exact = TRUE)$p.value}, group1, group2)
   return(data.frame("w" = w, "p" = p))
